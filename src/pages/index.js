17a1005299c681a21a421ba061a29d24de47342f
Kenny Gray <kenneth-gray@users.noreply.github.com>
2018-09-03 13:28:29 +0100

    Initial setup for github pages

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Open source libraries</h1>
+    <ul>
+      <li><Link to="/react-aria-accordion/">React Aria Accordion</Link></li>
+    </ul>
+  </div>
+)
+
+export default IndexPage
---
+++ src/pages/react-aria-accordion.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ReactAriaAccordion = () => (
+  <div>
+    <h1>React Aria Accordion</h1>
+    <Link to="/">Home</Link>
+  </div>
+)
+
+export default ReactAriaAccordion
